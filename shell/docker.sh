d35848b6e1de804d7678a7813fe46f74970be679
kujyp <swe.jaeyoungpark@navercorp.com>
2019-08-28 23:00:00 +0900

    A  shell/docker.sh

---
+++ shell/docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+(
+cd generated/dockerfiles/registry.navercorp.com/mtengine/cuda_base
+docker_tag=registry.navercorp.com/mtengine/cuda_base:cuda10.0_cudnn7.3
+docker -H 10.116.70.141:4243 build -t ${docker_tag} -f Dockerfile.cuda10.0_cudnn7.3 .
+)
+
+
+(
+cd generated/dockerfiles/registry.navercorp.com/mtengine/cuda_python_base
+docker_tag=registry.navercorp.com/mtengine/cuda_python_base:cuda10.0_cudnn7.3_python3.6.7
+docker -H 10.116.70.141:4243 build -t ${docker_tag} -f Dockerfile.cuda10.0_cudnn7.3_python3.6.7 .
+)
+
+(
+cd dockerfiles/registry.navercorp.com/mtengine/horovod
+docker_tag=registry.navercorp.com/mtengine/horovod:cuda10.0_cudnn7.3_python3.6.7_tf1.14.0
+docker -H 10.116.70.141:4243 build -t ${docker_tag} -f Dockerfile.cuda10.0_cudnn7.3_python3.6.7_tf1.14.0 .
+docker -H 10.116.70.141:4243 run --privileged -it --rm ${docker_tag}
+)
