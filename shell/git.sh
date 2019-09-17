48c1f03f525bf7b3a0020968a67b060cacd40a65
kujyp <swe.jaeyoungpark@navercorp.com>
2019-09-17 23:00:11 +0900

    M  shell/git.sh

--- shell/git.sh
+++ shell/git.sh
@@ -4,3 +4,7 @@ if [[ ! -z "$(git status --porcelain)" ]]; then
    echo "Generated files changed. Did you run 'tools/dockerfile_assembler.py'?"
    exit 1
 fi
+
+
+git tag -a "spocube_v1.7.5" -m "[스포큐브] v1.7.5"
+git push origin spocube_v1.7.5
