e708280f4497c4730fb59bc3b650c9dd8674e752
kujyp <swe.jaeyoungpark@navercorp.com>
2019-09-20 12:00:14 +0900

    M  shell/get_cuda_version.sh

--- shell/get_cuda_version.sh
+++ shell/get_cuda_version.sh
@@ -9,6 +9,17 @@ nvcc --version | grep "Cuda compilation" | awk '{ print $6 }'
 nvcc --version | grep "Cuda compilation" | awk '{ print $6 }' | tr -d [:alpha:]
 #> 10.0.130
 
+CUDA_VERSION_MAJOR=8
+CUDA_VERSION_MINOR=0
+CUDA_VERSION_PATCH=61
+CUDA_VERSION=${CUDA_VERSION_MAJOR}.${CUDA_VERSION_MINOR}.${CUDA_VERSION_PATCH}
+
+echo $CUDA_VERSION | cut -d '.' -f1 # == 8
+echo $CUDA_VERSION | cut -d '.' -f2 # == 0
+echo $CUDA_VERSION | cut -d '.' -f3 # == 61
+echo $CUDA_VERSION | cut -d '.' -f 1,2 # == 8.0
+echo $CUDA_VERSION | cut -d '.' -f 1,2,3 # ==== 8.0.61
+
 cat /usr/local/cuda-9.0/include/cudnn.h | grep "#define CUDNN_MAJOR" -A 2 | awk '{ print $3 }' | xargs | tr ' ' '.'
 cat /usr/local/cuda/include/cudnn.h | grep "#define CUDNN_MAJOR" -A 2 | awk '{ print $3 }' | xargs | tr ' ' '.'
 #> 7.3.1
@@ -18,3 +29,7 @@ cat /usr/local/cuda/include/cudnn.h | grep "#define CUDNN_MAJOR" -A 2 | awk '{ p
 
 #https://stackoverflow.com/a/9730706
 #How to get the cuda version?
+
+
+# Check nccl installation
+#locate nccl| grep "libnccl.so"
