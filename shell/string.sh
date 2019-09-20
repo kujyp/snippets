0b9426f94b55f0e0ef99905355678288aba6a919
kujyp <swe.jaeyoungpark@navercorp.com>
2019-09-20 14:00:14 +0900

    M  shell/string.sh

--- shell/string.sh
+++ shell/string.sh
@@ -13,4 +13,48 @@ answer=n123N
 echo ${answer%[nN]}
 # > n123
 
+### Trim question
+q=n123N?
+echo ${q%?}
+# > n123N
+
+q=n123N?1
+echo ${q%?}
+# > n123N?
+
+q=n123N?1
+echo ${q%\?}
+# > n123N?1
+
+q=n123N?
+echo ${q%\?}
+# > n123N
+
+
+
 echo ${email%%@example.com}
+
+
+answer=y
+test -z "${answer#[yYnN]}" && echo true
+# > true
+
+answer=Y
+test -z "${answer#[yYnN]}" && echo true
+# > true
+
+answer=n
+test -z "${answer#[yYnN]}" && echo true
+# > true
+
+answer=N
+test -z "${answer#[yYnN]}" && echo true
+# > true
+answer=No
+test -z "${answer#[yYnN]}" && echo true
+# > ""
+
+
+
+## Remove white space
+awk '{$1=$1};1'
