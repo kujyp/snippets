7a7528f8e9a0a85d37eaa091f7db596d0fbf48b4
kujyp <swe.jaeyoungpark@navercorp.com>
2019-11-09 18:00:13 +0900

    M  shell/sed.sh

--- shell/sed.sh
+++ shell/sed.sh
@@ -4,3 +4,8 @@ sed -i '' "s/^__version__.*/__version__ = '0.0.0'/" mtrobust/about.py
 # sed for Linux
 sed -i "s/^__version__.*/__version__ = '0.0.0'/" mtrobust/about.py
 cat mtrobust/about.py
+
+sed -i "s/cd-jenkins:80/35.243.115.38/" jenkins.model.JenkinsLocationConfiguration.xml
+
+docker run
+echo "010-1234-5678" | sed -nr ''
