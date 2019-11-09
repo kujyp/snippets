1bf9f28b24b4f978ae74e1bc41af091cecfd6a6d
kujyp <swe.jaeyoungpark@navercorp.com>
2019-11-09 19:00:15 +0900

    M  shell/sed.sh

--- shell/sed.sh
+++ shell/sed.sh
@@ -7,5 +7,30 @@ cat mtrobust/about.py
 
 sed -i "s/cd-jenkins:80/35.243.115.38/" jenkins.model.JenkinsLocationConfiguration.xml
 
-docker run
-echo "010-1234-5678" | sed -nr ''
+
+# shell phonenumber validation
+# Ref: https://stackoverflow.com/questions/21112707/check-if-a-string-matches-a-regex-in-bash-script
+# Ref2: https://soooprmx.com/archives/7718
+docker run -it --rm centos:7
+function isPhoneNumberThenEchoYes() {
+    if [[ "${1-}" =~ ^([0-9]{2}|[0-9]{3})(-| )?([0-9]{3}|[0-9]{4})(-| )?[0-9]{4}$ ]]; then
+        echo yes
+    else
+        echo no
+    fi
+}
+
+isPhoneNumberThenEchoYes "0"
+isPhoneNumberThenEchoYes "a"
+
+isPhoneNumberThenEchoYes "01012345678"
+isPhoneNumberThenEchoYes "010-1234-5678"
+isPhoneNumberThenEchoYes "010-12345678"
+isPhoneNumberThenEchoYes "010 1234 5678"
+isPhoneNumberThenEchoYes "010 123 5678"
+isPhoneNumberThenEchoYes "02 123 5678"
+isPhoneNumberThenEchoYes "02 1234 5678"
+isPhoneNumberThenEchoYes "aa1234 5678"
+
+if [[ -z $(echo "010-1234-5678" | sed -nr 's/^([0-9]{2}|[0-9]{3})(-| )?([0-9]{3}|[0-9]{4})(-| )?[0-9]{4}$//g') ]]; then echo yes; else echo no; fi
+if [[ -z $(echo "010-1234-5678" | sed -nr 's/^([0-9]{2}|[0-9]{3})(-| )?([0-9]{3}|[0-9]{4})(-| )?[0-9]{4}$//g') ]]; then echo yes; else echo no; fi
