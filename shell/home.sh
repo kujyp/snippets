ab2d10f72aec98f4dfe1d7f9d3729c03ff0b5851
kujyp <swe.jaeyoungpark@navercorp.com>
2019-11-18 20:41:15 +0900

    M  shell/home.sh

--- shell/home.sh
+++ shell/home.sh
@@ -7,3 +7,10 @@ eval echo ~
 #> /home/irteam/users/jaeyoung
 eval echo ~$USER
 #> /home1/irteam
+
+eval echo ~$(whoami)
+# -> docker centos 이미지에서 root 계정으로 시도했더니 $USER 가 비어있었음.
+# $(whoami) 로 대체
+# https://unix.stackexchange.com/a/76356
+# -> login 을 해야 USER 란이 채워지는듯함.
+
