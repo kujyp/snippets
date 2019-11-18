828d5b5712bc67a1c980e5f646732f0904119941
kujyp <swe.jaeyoungpark@navercorp.com>
2019-11-18 20:31:25 +0900

    A  shell/home.sh

---
+++ shell/home.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# q: linux get home path
+# Get home directory by username
+# Ref: https://superuser.com/a/484280
+eval echo ~
+#> /home/irteam/users/jaeyoung
+eval echo ~$USER
+#> /home1/irteam
